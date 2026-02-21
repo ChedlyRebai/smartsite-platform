53ff45e9c710aa0d20548efd106785446f2454e6
rebaiChedly <107418294+ChedlyRebai@users.noreply.github.com>
2026-02-21 15:31:08 +0100

    feat(auth): add modals for adding permissions, roles, and users

---
+++ apps/frontend/src/app/provider/ModalProvider/AddPermissionModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddPermissionModal = () => {
+  return (
+    <div>AddPermissionModal</div>
+  )
+}
+
+export default AddPermissionModal
---
+++ apps/frontend/src/app/provider/ModalProvider/AddRoleModal.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AddRoleModal = () => {
+  return (
+    <div>AddRoleModal</div>
+  )
+}
+
+export default AddRoleModal
