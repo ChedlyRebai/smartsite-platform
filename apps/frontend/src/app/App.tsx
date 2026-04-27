1b460b66cd084cdfa80968563e64030381703cf5
Mahmoud Hasnaoui <mahmoudhasnaoui223@gmail.com>
2026-04-27 15:04:27 +0100

    ---

--- apps/frontend/src/app/App.tsx
+++ apps/frontend/src/app/App.tsx
@@ -4,7 +4,7 @@ import { router } from "./routes";
 import ModalProvider from "./provider/ModalProvider";
 import ThemeSync from "./components/ThemeSync";
 import { ThemeProvider } from "./context/ThemeContext";
-
+import "stream-chat-react/dist/css/v2/index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { GlobalAccessibilityBar } from "./components/accessibility/GlobalAccessibilityBar";
 
